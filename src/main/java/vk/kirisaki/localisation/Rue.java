b9467ebe489524bb97cad5266a0068041f820f6b
kirisaki_vk <kirisaki.vk@gmail.com>
2024-06-17 12:02:53 +0300

    feat: new Rue

---
+++ src/main/java/vk/kirisaki/localisation/Rue.java
@@ -0,0 +1,13 @@
+package vk.kirisaki.localisation;
+
+public final class Rue {
+    private final String nom;
+
+    public Rue(String nom) {
+        this.nom = nom;
+    }
+
+    public Rue() {
+        this.nom = "";
+    }
+}
